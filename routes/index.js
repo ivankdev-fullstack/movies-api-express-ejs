910bc891e0f579104fa42daa818d04aef975a4ab
ivankdev-fullstack <ivan.kononenko.dev@gmail.com>
2025-02-07 16:36:45 +0200

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const request = require("request");
+const router = express.Router();
+
+const { API_BASE_URL, API_KEY } = process.env;
+
+const nowPlayingUrl = `${API_BASE_URL}/movie/now_playing?api_key=${API_KEY}`;
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  request.get(nowPlayingUrl, (err, res, data) => {
+    const parsedData = JSON.parse(data);
+  });
+  res.render("index", { title: "Express" });
+});
+
+module.exports = router;
