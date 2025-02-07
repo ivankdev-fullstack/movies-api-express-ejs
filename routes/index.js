deb32df59eb0b3a563e93a3732087fa5d5750e5a
ivankdev-fullstack <ivan.kononenko.dev@gmail.com>
2025-02-07 16:57:31 +0200

    feat: add dynamic view for movies index page

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,21 @@ const express = require("express");
 const request = require("request");
 const router = express.Router();
 
-const { API_BASE_URL, API_KEY } = process.env;
-
+const { API_BASE_URL, API_KEY, IMAGE_BASE_URL } = process.env;
 const nowPlayingUrl = `${API_BASE_URL}/movie/now_playing?api_key=${API_KEY}`;
 
+router.use((req, res, next) => {
+  res.locals.imageBaseUrl = IMAGE_BASE_URL;
+  next();
+});
+
 router.get("/", function (req, res, next) {
-  request.get(nowPlayingUrl, (err, res, data) => {
+  request.get(nowPlayingUrl, (err, response, data) => {
     const parsedData = JSON.parse(data);
+    res.render("index", {
+      movies: parsedData.results,
+    });
   });
-  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
